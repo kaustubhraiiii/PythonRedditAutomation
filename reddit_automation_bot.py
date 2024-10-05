dac12d6f3f6c9c6385016f97ab310cd05a091357
Kaustubh Rai <hello@kaustbhrai.com>
2024-10-05 09:08:38 -0400

    Added Reddit API setup

---
+++ reddit_automation_bot.py
@@ -0,0 +1,15 @@
+import praw
+import time
+import csv
+import os
+
+
+# setting up Reddit API connection
+reddit = praw.reddit(
+    client_id='R6tP7GiTPo9VYvOCHhWzBA',
+    client_secret='uoh0BvIoIET9KO_DqQG0cyrIRKl8tg',
+    user_agent='pythonAutomation  by /u/kaustubhraii',
+    username='kaustubhraii',
+    password='rAIPARIVAR1',
+)
+
